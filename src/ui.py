055926249764074faa33eb9284770cb5eb13349b
reenbe <uni.sargsyan@gmail.com>
2026-06-08 16:31:08 +1000

    ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,12 +1,10 @@
-
 import pygame
 import random
-from config import *
+from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 class MovingStar:
     """Класс для движущейся звезды"""
-
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -39,103 +37,90 @@ class MovingStar:
 
 class Menu:
     """Класс главного меню"""
-
+    
     def __init__(self):
         self.font_title = pygame.font.Font(None, 74)
         self.font_button = pygame.font.Font(None, 36)
-
+        
         self.white = (255, 255, 255)
         self.gray = (150, 150, 150)
-
+        
         self.start_hover = False
         self.exit_hover = False
-
-        self.stars = []
+        
+        # Используем константы из config
         self.screen_width = SCREEN_WIDTH
         self.screen_height = SCREEN_HEIGHT
-
+        
+        # Создаем звезды сразу
+        self.stars = [MovingStar(self.screen_width, self.screen_height) for _ in range(150)]
+        
+        # Создаем кнопки
+        self.start_button = pygame.Rect(
+            self.screen_width // 2 - 100, 
+            self.screen_height // 2 - 30, 
+            200, 50
+        )
+        self.exit_button = pygame.Rect(
+            self.screen_width // 2 - 100, 
+            self.screen_height // 2 + 40, 
+            200, 50
+        )
+    
     def update_stars(self, dt):
-        if not self.stars:
-            self.stars = [MovingStar(self.screen_width, self.screen_height) for _ in range(150)]
+        """Обновление звезд"""
         for star in self.stars:
             star.update(dt)
-
-    def set_screen_size(self, width, height):
-        self.screen_width = width
-        self.screen_height = height
-        self.stars = [MovingStar(width, height) for _ in range(150)]
-
+    
     def handle_event(self, event, mouse_pos):
+        """Обработка событий в меню"""
+        # Обновляем hover при движении мыши
         if event.type == pygame.MOUSEMOTION:
-            start_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 - 30, 200, 50)
-            exit_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 + 40, 200, 50)
-            self.start_hover = start_button.collidepoint(mouse_pos)
-            self.exit_hover = exit_button.collidepoint(mouse_pos)
-
+            self.start_hover = self.start_button.collidepoint(mouse_pos)
+            self.exit_hover = self.exit_button.collidepoint(mouse_pos)
+        
+        # Обработка клика
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            start_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 - 30, 200, 50)
-            exit_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 + 40, 200, 50)
-            if start_button.collidepoint(mouse_pos):
+            if self.start_button.collidepoint(mouse_pos):
                 return "start"
-            elif exit_button.collidepoint(mouse_pos):
+            elif self.exit_button.collidepoint(mouse_pos):
                 return "exit"
-
+        
+        # Обработка клавиш
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 return "start"
             elif event.key == pygame.K_ESCAPE:
                 return "exit"
-
+        
         return None
-
+        
     def render(self, screen):
-        if not self.stars:
-            self.stars = [MovingStar(self.screen_width, self.screen_height) for _ in range(150)]
-
+        """Отрисовка меню"""
+        # Рисуем звезды
         for star in self.stars:
             star.draw(screen)
-
-        start_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 - 30, 200, 50)
-        exit_button = pygame.Rect(self.screen_width // 2 - 100, self.screen_height // 2 + 40, 200, 50)
-
+        
+        # Заголовок
         title = self.font_title.render("VOID HUNTER", True, self.white)
         title_rect = title.get_rect(center=(self.screen_width // 2, self.screen_height // 4))
         screen.blit(title, title_rect)
-
+        
+        # Кнопка START
         start_color = self.gray if self.start_hover else self.white
-        pygame.draw.rect(screen, start_color, start_button, 2)
+        pygame.draw.rect(screen, start_color, self.start_button, 2)
         start_text = self.font_button.render("START", True, start_color)
-        start_text_rect = start_text.get_rect(center=start_button.center)
+        start_text_rect = start_text.get_rect(center=self.start_button.center)
         screen.blit(start_text, start_text_rect)
-
+        
+        # Кнопка EXIT
         exit_color = self.gray if self.exit_hover else self.white
-        pygame.draw.rect(screen, exit_color, exit_button, 2)
+        pygame.draw.rect(screen, exit_color, self.exit_button, 2)
         exit_text = self.font_button.render("EXIT", True, exit_color)
-        exit_text_rect = exit_text.get_rect(center=exit_button.center)
+        exit_text_rect = exit_text.get_rect(center=self.exit_button.center)
         screen.blit(exit_text, exit_text_rect)
-
+        
+        # Подсказка
         hint = self.font_button.render("Press SPACE to start", True, self.gray)
         hint_rect = hint.get_rect(center=(self.screen_width // 2, self.screen_height - 50))
         screen.blit(hint, hint_rect)
-
-
-
-class UI:
-    """Класс игрового интерфейса"""
-    
-    def __init__(self):
-        # TODO: создать шрифты
-        pass
-        
-    def render(self, screen, player, score):
-        """Отрисовка игрового UI"""
-        # TODO: нарисовать HP игрока
-        # TODO: нарисовать счет
-        pass
-        
-    def render_game_over(self, screen, score):
-        """Отрисовка экрана Game Over"""
-        # TODO: нарисовать "GAME OVER"
-        # TODO: нарисовать финальный счет
-        # TODO: нарисовать подсказку "Press R to restart"
-        pass
